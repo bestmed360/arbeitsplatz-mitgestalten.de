60a9fef534094f42ff3062cb93940fb27190267d
Julian Koehn <julian.koehn@ionos.com>
2024-04-10 21:33:58 +0200

    feat: alpine

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import sitemap from "@astrojs/sitemap";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://arbeitsplatz-mitgestalten.de",
-  integrations: [tailwind(), sitemap()],
-  output: "static",
+  integrations: [tailwind(), sitemap(), alpinejs()],
+  output: "static"
   //   adapter: netlify(),
 });
